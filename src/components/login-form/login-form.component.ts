550647376a78cced6b8ca364aa492d79fc264118
moneychaudhary5 <moneychaudhary5@gmail.com>
2017-07-24 17:16:46 +0530

    added form validations in login and register form

--- src/components/login-form/login-form.component.ts
+++ src/components/login-form/login-form.component.ts
@@ -2,6 +2,7 @@ import {Component, EventEmitter, Output} from '@angular/core';
 import {Account} from "../../models/account.interface";
 import {LoginResponse} from "../../models/login-response.interface";
 import {AuthService} from "../../providers/auth.serivce";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login-form',
@@ -12,11 +13,17 @@ export class LoginFormComponent {
 
   account = {}  as Account;
   registerPage:string = 'RegisterPage';
+  loginFormGroup:FormGroup;
+
 
   @Output() loginStatus: EventEmitter<LoginResponse>;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private formBuilder:FormBuilder) {
     this.loginStatus = new EventEmitter<LoginResponse>();
+    this.loginFormGroup =  this.formBuilder.group({
+      email : ['',Validators.compose([Validators.required,Validators.email])],
+      password : ['',Validators.compose([Validators.required,Validators.minLength(6)])]
+    });
   }
 
 
--- src/components/register-form/register-form.component.ts
+++ src/components/register-form/register-form.component.ts
@@ -2,6 +2,7 @@ import {Component, EventEmitter, Output} from '@angular/core';
 import {Account} from "../../models/account.interface";
 import {AuthService} from "../../providers/auth.serivce";
 import {LoginResponse} from "../../models/login-response.interface";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register-form',
@@ -11,15 +12,27 @@ export class RegisterFormComponent {
 
   account = {} as Account;
   @Output() registerStatus:EventEmitter<LoginResponse>;
+  registerFormGroup:FormGroup;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,private formBuilder:FormBuilder) {
     this.registerStatus = new EventEmitter<LoginResponse>();
+    this.registerFormGroup = this.formBuilder.group({
+      email : ['',Validators.compose([Validators.required,Validators.email])],
+      password : ['',Validators.compose([Validators.required,Validators.minLength(6)])],
+      confirmPassword : ['',Validators.compose([Validators.required,this.validatePasswordConfirmation.bind(this)])]
+    });
   }
 
 
   async onSignUp() {
       const result = await this.authService.signUpWithEmailandPassword(this.account);
       this.registerStatus.emit(result);
+  }
+
+  validatePasswordConfirmation(control: FormControl): any {
+    if(this.registerFormGroup) {
+      return control.value === this.registerFormGroup.get('password').value ? null : { notSame: true}
+    }
   }
 
 }
